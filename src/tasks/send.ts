5ddc665e29a10985c8e90d76d2ff44415cde9694
Yunjong Jeong <yunjong@kaist.ac.kr>
2024-01-12 00:26:50 +0900

    send: try-catch parseLog

--- src/tasks/send.ts
+++ src/tasks/send.ts
@@ -1,10 +1,16 @@
 import { task } from "hardhat/config";
+import { Log } from "@ethersproject/abstract-provider";
 import _ from "lodash";
 
 import { FromArgType, resolveFuncArgs, normalizeCallResult, normalizeRpcResult } from "../helpers";
 import "../type-extensions";
 
 export const TASK_SEND = "send";
+
+interface ParsedLog extends Log {
+  eventName?: string;
+  eventArgs?: string;
+}
 
 task(TASK_SEND, "Send a transaction to a contract")
   .addOptionalParam("from", "Caller address or index", 0, FromArgType)
@@ -15,13 +21,13 @@ task(TASK_SEND, "Send a transaction to a contract")
   .addPositionalParam("func", "Function name or signature (example: 'number()', 'balanceOf(address)')")
   .addVariadicPositionalParam("args", "call arguments", [])
   .setAction(async (taskArgs) => {
-		const { unsigned, json, name, func } = taskArgs;
-		const { contract, sender, unsignedTx } = await resolveFuncArgs(taskArgs);
+    const { unsigned, json, name, func } = taskArgs;
+    const { contract, sender, unsignedTx } = await resolveFuncArgs(taskArgs);
 
-		if (unsigned) {
-			console.log(normalizeRpcResult(unsignedTx));
-			return;
-		}
+    if (unsigned) {
+      console.log(normalizeRpcResult(unsignedTx));
+      return;
+    }
 
     const tx = await sender.sendTransaction(unsignedTx);
     if (!json) {
@@ -29,10 +35,12 @@ task(TASK_SEND, "Send a transaction to a contract")
     }
 
     const rc = await tx.wait();
-    _.each(rc.logs, (log) => {
-      const desc = contract.interface.parseLog(log);
-      (log as any).eventName = desc.signature;
-      (log as any).eventArgs = normalizeCallResult(desc.args);
+    _.each(rc.logs, (log: ParsedLog) => {
+      try {
+        const desc = contract.interface.parseLog(log);
+        log.eventName = desc.signature;
+        log.eventArgs = normalizeCallResult(desc.args);
+      } catch (e) {}
     });
 
     if (json) {
@@ -40,8 +48,10 @@ task(TASK_SEND, "Send a transaction to a contract")
     } else {
       const status = rc.status == 1 ? "ok" : `failed (status=${rc.status})`;
       console.log(`${status} (block ${rc.blockNumber}, gas used: ${rc.gasUsed.toString()})`);
-      _.each(rc.logs, (log) => {
-        console.log("emit", (log as any).eventName, JSON.stringify((log as any).eventArgs, null, 2));
+      _.each(rc.logs, (log: ParsedLog) => {
+        if (log.eventName) {
+          console.log("emit", log.eventName, JSON.stringify(log.eventArgs, null, 2));
+        }
       });
     }
   });
