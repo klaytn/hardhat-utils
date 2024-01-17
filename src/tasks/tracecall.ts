56d180cca7f5a9547b18c570be75eb85176229f7
ollie.j <ollie.j@klaytn.foundation>
2024-01-17 19:35:51 +0900

    trace: Support stackup tracers

--- src/tasks/tracecall.ts
+++ src/tasks/tracecall.ts
@@ -10,19 +10,20 @@ export const TASK_TRACECALL = "tracecall";
 task(TASK_TRACECALL, "debug_traceCall a transaction")
   .addOptionalParam("from", "Caller address or index", 0, FromArgType)
   .addOptionalParam("to", "Target address. Loaded from deployments if empty.", "")
-  .addOptionalParam("tracer", "tracer type (call,revert,struct,stackup)", "call")
+  .addOptionalParam("tracer", "tracer type (call,revert,struct,stackup,stackupcol,stackupexe)", "call")
   .addOptionalParam("limit", "(only if --tracer struct) maximum number of opcodes", "")
   .addOptionalParam("timeout", "trace timeout in Go duration format", "5s")
+  .addOptionalParam("block", "block number to run the trace on", "latest")
   .addFlag("json", "Print raw trace in json")
   .addFlag("withContext", "(only if --json --tracer struct) include memory, stack, storage")
   .addPositionalParam("name", "Contract name (example: 'Counter', 'src/Lock.sol:Lock')")
   .addPositionalParam("func", "Function name or signature (example: 'number()', 'balanceOf(address)')")
   .addVariadicPositionalParam("args", "call arguments", [])
   .setAction(async (taskArgs) => {
-    const { tracer, json } = taskArgs;
+    const { tracer, block, json } = taskArgs;
     const { unsignedTx } = await resolveFuncArgs(taskArgs);
 
-    const trace = await traceCall(unsignedTx, taskArgs);
+    const trace = await traceCall(unsignedTx, block, taskArgs);
   
     if (json) {
       console.log(JSON.stringify(trace, null, 2));
--- src/tasks/tracetx.ts
+++ src/tasks/tracetx.ts
@@ -8,7 +8,7 @@ import "../type-extensions";
 export const TASK_TRACETX = "tracetx";
 
 task(TASK_TRACETX, "debug_traceTransaction a transaction")
-  .addOptionalParam("tracer", "tracer type (call,revert,struct,stackup)", "call")
+  .addOptionalParam("tracer", "tracer type (call,revert,struct,stackupcol,stackupexe)", "call")
   .addOptionalParam("limit", "(only if --tracer struct) maximum number of opcodes", "")
   .addOptionalParam("timeout", "trace timeout in Go duration format", "5s")
   .addFlag("json", "Print raw trace in json")
